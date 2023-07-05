1347594d48ffe48e000f1636bf5716551701e118
ugoswami11 <ugoswami1112@gmail.com>
2023-07-05 23:24:54 +0530

    api-cors-func-index-serverless-v2.1.1

--- api/index.js
+++ api/index.js
@@ -17,6 +17,30 @@ app.use(cookieParser());
 
 mongoose.connect('mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASS+'@'+process.env.DB_HOST);
 
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+}
+  
+const handler = (req, res) => {
+    const d = new Date()
+    res.end(d.toString())
+}
+  
+module.exports = allowCors(handler)
+
 app.post('/register', async function(req, res){
     const {username, password} = req.body;
     
