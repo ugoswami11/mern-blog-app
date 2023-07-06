e9c441867701749cdee6c2ed20dcd20f4f3eea90
ugoswami11 <ugoswami1112@gmail.com>
2023-07-06 23:09:28 +0530

    client-no-cors-update-v2.1.2.3

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -5,7 +5,7 @@ export default function Homepage(){
     const [posts, setPosts] = useState([]);
 
     useEffect(()=>{
-        fetch('https://mern-blog-api-git-main-ugoswami11.vercel.app/post').then( response => {
+        fetch('https://mern-blog-api-git-main-ugoswami11.vercel.app/post',{mode: "no-cors"}).then( response => {
             response.json().then(posts => {
                 setPosts(posts);
             });
--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -10,7 +10,7 @@ export default function PostPage(){
     const {id} = useParams();
 
     useEffect(() => {
-        fetch(`https://mern-blog-api-git-main-ugoswami11.vercel.app/post/${id}`).then(
+        fetch(`https://mern-blog-api-git-main-ugoswami11.vercel.app/post/${id}`,{mode: "no-cors"}).then(
             response => {
                 response.json().then(postInfo =>{
                     setPostInfo(postInfo);
